40b26941a66d937200f25a2a855c27e372cf3fb9
argonlefou <argonlefou@gmail.com>
2022-05-23 22:55:16 +0200

    Added unity plugin source[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D Wild West Shootout [2~plugin source[D[D[D[D[D[D[D[D[D[D[D[D[D[D Unity plugin source

---
+++ UnityPlugin_BepInEx_WWS/mGameBeginUIController.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace UnityPlugin_BepInEx_WWS
+{
+    class mGameBeginUIController
+    {
+        /// <summary>
+        /// Removing mouse click on Title Screen to start game
+        /// </summary>
+        [HarmonyPatch(typeof(GameBeginUIController), "Update")]
+        class Update
+        {
+            static bool Prefix()
+            {                
+                return false;
+            }
+        }
+    }
+}
---
+++ UnityPlugin_BepInEx_WWS/mShowGamePointController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UnityPlugin_BepInEx_WWS
+{
+    class mShowGamePointController
+    {
+        /// <summary>
+        /// Displaying cursors on Level Select screen
+        /// </summary>
+        [HarmonyPatch(typeof(ShowGamePointController), "Update")]
+        class Update
+        {
+            static bool Prefix(ShowGamePointController __instance)
+            {
+                for (int index = 0; index < __instance.gunPoint.Length; ++index)
+                { 
+                    if (GameData.GetPlayerData(index).CanShoot())
+                    {
+                        int r = Demulshooter_Plugin.WWS_Mmf.ReadAll();
+                        if (r == 0)
+                        {
+                            Vector3 vAxis = new Vector3();
+                            vAxis.x = BitConverter.ToSingle(Demulshooter_Plugin.WWS_Mmf.Payload, WWS_MemoryMappedFile_Controller.INDEX_P1_UISCREEN_X + (16 * index));
+                            vAxis.y = BitConverter.ToSingle(Demulshooter_Plugin.WWS_Mmf.Payload, WWS_MemoryMappedFile_Controller.INDEX_P1_UISCREEN_Y + (16 * index));
+                            __instance.gunPoint[index].mRectTF.anchoredPosition = vAxis;
+                        }
+                        else
+                        {
+                            UnityEngine.Debug.LogError("mShowGamePointController.Update() => DemulShooter MMF read error : " + r.ToString());
+                            __instance.gunPoint[index].transform.localPosition = new Vector3(-1000f, -1000f);
+                        }
+                    }
+                    else
+                        __instance.gunPoint[index].transform.localPosition = new Vector3(-1000f, -1000f);
+                }
+                return false;
+            }
+        }
+    }
+}
